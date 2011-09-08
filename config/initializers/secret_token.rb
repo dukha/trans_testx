7c7b676decb0cd04a53de3291b92066c254e545c
dukha <mplennon@gmail.com>
2011-09-08 16:18:14 +1000

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TransTestx::Application.config.secret_token = '7fb1865411f878dba4681ec0ffe8a2b6cd7f8b177605a8397859b84942d35aee0b36b4e353458053a725da60c6f9febb1cc21ec7e2462bfcd4068b8d5a8d5bd0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TransTestx::Application.config.session_store :cookie_store, :key => '_trans_testx_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TransTestx::Application.config.session_store :active_record_store
